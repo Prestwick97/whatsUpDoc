2f63256af5e21658deed30d84fb11c54a8cb824d
Prestwick97 <prestwick97@gmail.com>
2020-02-14 12:21:30 -0800

    add basic UI skeleton

--- src/main.js
+++ src/main.js
@@ -3,4 +3,26 @@ import $ from 'jquery';
 import 'bootstrap';
 import './styles.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-// import {  } from './project-name';
+import { DocService } from './API-call';
+
+$(document).ready(function() {
+
+  $('#doctorFind').submit(function(event) {
+const userSearch = $('#docSearch').val();
+
+    (async () => {
+      let docService = new DpcService();
+      const response = await docService.getDocByName();
+      getElements(response);
+    })();
+
+    function getElements(response) {
+      if (response) {
+        $('').text(``);
+      } else {
+        $('').text(`There was an error handling your request.`);
+      }
+    }
+
+  });
+});
