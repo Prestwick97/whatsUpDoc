28b6d8b2444e4c74cd1c83f63200f9959255ce1e
Prestwick97 <prestwick97@gmail.com>
2020-02-22 16:12:11 -0800

    add respetive labels for output in html

--- src/main.js
+++ src/main.js
@@ -18,14 +18,22 @@ $(document).ready(function() {
     })();
 
     function getElements(response) {
+      console.log(response);
       for (let i = 0; i<=9; i++) {
-        let docName = `${response.data[i].practices[0].name}`;
+        let docFirstName = `${response.data[i].pofile[0].first_name}`;
+        let docLastName = `${response.data[i].pofile[0].last_name}`;
         let docPatients = `${response.data[i].practices[0].accepts_new_patients}`;
         let docWeb = `${response.data[i].practices[0].website}`;
         let docNum = `${response.data[i].practices[0].phones[0].number}`;
+        let docImg = `${response.data[i].profile.image_url}`;
         console.log(docName + docPatients + docWeb + docNum);
         if (response) {
-          $('#docResult').append(docName + ' ' + docPatients + ' ' + docWeb + ' ' + docNum);
+          $('#docImg').append("<img src = " + docImg +">" + "</img>");
+          $('#firstName').append(docFirstName);
+          $('#lastName').append(docLastName);
+          $('#docPatients').append(docPatients);
+          $('#docWeb').append();
+          $('#docNum').append();
         } else {
           $('#docResult').text(`There was an error handling your request.`);
         }
