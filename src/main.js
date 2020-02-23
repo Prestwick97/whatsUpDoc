54e442d63b9c29b338a500a9f9374c9dd6501b06
Prestwick97 <prestwick97@gmail.com>
2020-02-22 16:36:13 -0800

    add proper styling for output

--- src/main.js
+++ src/main.js
@@ -19,21 +19,22 @@ $(document).ready(function() {
 
     function getElements(response) {
       console.log(response);
-      for (let i = 0; i<=9; i++) {
-        let docFirstName = `${response.data[i].pofile[0].first_name}`;
-        let docLastName = `${response.data[i].pofile[0].last_name}`;
+      for (let i = 0; i<4; i++) {
+        let docFirstName = `${response.data[i].profile.first_name}`;
+        let docLastName = `${response.data[i].profile.last_name}`;
         let docPatients = `${response.data[i].practices[0].accepts_new_patients}`;
         let docWeb = `${response.data[i].practices[0].website}`;
         let docNum = `${response.data[i].practices[0].phones[0].number}`;
         let docImg = `${response.data[i].profile.image_url}`;
-        console.log(docName + docPatients + docWeb + docNum);
+        let docProfile = "<img src = " + docImg +">" + "</img>" +"<br>" + "First name: " + docFirstName + "<br>" + "Last name: " + docLastName + "<br>" + "Accepting new patients: " + docPatients + "<br>" + "Website: " + docWeb + "<br>" +"Phone: " + docNum + "<br>";
+        // console.log(docName + docPatients + docWeb + docNum);
         if (response) {
-          $('#docImg').append("<img src = " + docImg +">" + "</img>");
-          $('#firstName').append(docFirstName);
-          $('#lastName').append(docLastName);
-          $('#docPatients').append(docPatients);
-          $('#docWeb').append();
-          $('#docNum').append();
+          // $('.docImg').append("<img src = " + docImg +">" + "</img>");
+          // $('.firstName').append(docFirstName);
+          // $('.lastName').append(docLastName);
+          // $('.docPatients').append(docPatients);
+          // $('.docWeb').append(docWeb);
+          $('#docProfile').append(docProfile);
         } else {
           $('#docResult').text(`There was an error handling your request.`);
         }
